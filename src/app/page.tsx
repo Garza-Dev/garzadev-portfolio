32f3a121330af52fe636bcf837abdcc2882f3d70
Eduardo Jr Garza <egarzajr.00@gmail.com>
2024-06-12 20:45:27 -0500

    layout testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <div style={{position: 'absolute', bottom: '0', width: '100%', height: '50px'}}>
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
         <Waves />
       </div>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,12 +10,12 @@ const Hero: React.FC = () => {
                 <div className="flex-1 md:pr-8">
                     <div className={styles.heroName}>Eduardo Garza</div>
                     <svg width="100%" height="2.4" viewBox="0 0 100 2" preserveAspectRatio="none">
-                        <line x1="30" y1="1" x2="70" y2="1" stroke="#FE5F55" stroke-width="3" />
+                        <line x1="30" y1="1" x2="70" y2="1" stroke="#FE5F55" strokeWidth="3" />
                     </svg>
                     <p className={styles.heroTxt}>Game Developer | Web Developer | Musician</p>
                 </div>
                 <div className={styles.imgContainer}>
-                    <Image src={EduardoGarzaIMG.src} layout='fill' objectFit='cover' alt='Eduardo Garza' />
+                    <Image src={EduardoGarzaIMG.src} layout='fill' objectFit='cover' className={styles.img} alt='Eduardo Garza' />
                 </div>
             </div>
         </main>
