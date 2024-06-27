12830a129730782bb08d6dcba0a63c991aa6b7f6
Garza-Dev <egarzajr.00@gmail.com>
2024-06-27 12:38:28 -0500

    scale testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata, Viewport } from "next";
 import Navbar from "@/components/Navbar/Navbar";
 import "./globals.css";
 import Head from "next/head";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Garza-Dev | Eduardo Garza",
   description: "Developed by Eduardo Garza",
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1
 };
 
 export default function RootLayout({
@@ -24,7 +27,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />  
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100..900&display=swap" rel="stylesheet" />
       </Head>
-      <body className={inter.className}>
+      <body>
         <div className='sticky top-0' style={{ zIndex: 10 }}>
           <Navbar />
         </div>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,13 +7,13 @@ import Waves from '../Waves/Waves';
 const Hero: React.FC = () => {
     return (
         <section className={styles.hero}>
-            <div className="mx-auto flex flex-col md:flex-row items-center p-8">
-                <div className="flex-1 md:pr-8">
+            <div>
+                <div>
                     <div className={styles.heroName}>Eduardo Garza</div>
                     <svg width="100%" height="2.4" viewBox="0 0 100 2" preserveAspectRatio="none">
                         <line x1="30" y1="1" x2="70" y2="1" stroke="#FE5F55" strokeWidth="3" />
                     </svg>
-                    <p className={styles.heroTxt}>Game Developer | Web Developer | Musician</p>
+                    <p className={styles.heroTxt}>Game Developer | Full-Stack Developer | Musician</p>
                 </div>
                 <div className={styles.imgContainer}>
                     <Image src={EduardoGarzaIMG.src} layout='fill' objectFit='cover' className={styles.img} alt='Eduardo Garza' />
