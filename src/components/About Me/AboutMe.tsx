69246dda10702500ef68afc0310d651635972304
Eduardo Jr Garza <egarzajr.00@gmail.com>
2024-07-01 22:26:56 -0500

    added pipes and logos to about me section

--- src/components/About Me/AboutMe.tsx
+++ src/components/About Me/AboutMe.tsx
@@ -1,33 +1,55 @@
 import styles from './AboutMe.module.css';
+import iconPipeIMG from '../../../public/iconPipe.png';
+import angularIcon from '../../../public/angularLogo.png';
+import azureIcon from '../../../public/azureLogo.png';
+import godotIcon from '../../../public/godotLogo.png';
+import sqlLogo from '../../../public/sqlLogo.png';
+import nextjsIcon from '../../../public/Next.js.png';
+import nodejsIcon from '../../../public/nodeJsLogo.png';
+import reactIcon from '../../../public/reactLogo.png';
+import unityIcon from '../../../public/unityLogo.png';
 
 const AboutMe: React.FC = () => {
     return (
-        <section id="about" className={styles.about}>
-            <h2>About Me</h2>
-            <p>
-                Hello, My name is Jack Parsons and I am a fourth-year Computer Science Student attending the University of Alberta.
-            </p>
-            {/* More content here */}
+        <section className={`${styles.main}`}>
+            <div className={`${styles.container} container mx-auto`}>
+                <div className={styles.iconContainer}>
+                    <img className={styles.pixelArtTop} src={iconPipeIMG.src} alt='Icon-Pipe' width={144} height={114} />
+                    <div className={styles.logosContainer}>
+                        <img className={styles.logo} src={reactIcon.src} alt='ReactIcon' width={105} height={105} />
+                        <img className={styles.logo} src={azureIcon.src} alt='AzureIcon' width={105} height={105} />
+                        <img className={styles.logo} src={godotIcon.src} alt='GodotIcon' width={105} height={105} />
+                        <img className={styles.logo} src={sqlLogo.src} alt='SQLIcon' width={105} height={105} />
+                    </div>
+                    <img className={styles.pixelArtBtm} src={iconPipeIMG.src} alt='Icon-Pipe' width={144} height={114} />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.heading}>About Me</h1>
+                    <p>
+                        Very long text goes here copy and paste this
+                        Very long text goes here copy and paste this
+                        Very long text goes here copy and paste this
+                        Very long text goes here copy and paste this
+                        Very long text goes here copy and paste this
+                        Very long text goes here copy and paste this
+                        Very long text goes here copy and paste this
+                        Very long text goes here copy and paste this
+                        Very long text goes here copy and paste this
+                        Very long text goes here copy and paste this
+                    </p>
+                </div>
+                <div className={styles.iconContainer}>
+                    <img className={styles.pixelArtTop} src={iconPipeIMG.src} alt='Icon-Pipe' width={64} height={64} />
+                    <div className={styles.logosContainer}>
+                        <img src={nextjsIcon.src} alt='NextJsIcon' width={105} height={105} />
+                        <img src={nodejsIcon.src} alt='NodeJSIcon' width={105} height={105} />
+                        <img src={angularIcon.src} alt='AngularIcon' width={105} height={105} />
+                        <img src={unityIcon.src} alt='UnityIcon' width={105} height={105} />
+                    </div>
+                    <img className={styles.pixelArtBtm} src={iconPipeIMG.src} alt='Icon-Pipe' width={64} height={64} />
+                </div>
+            </div>
         </section>
-        // <main className={`${styles.main} container mx-auto px-19`}>
-        //     <div className={styles.heading}>
-        //         <h1>About Me</h1>
-        //     </div>
-        //     <div>
-        //         <p>
-        //             Very long text goes here copy and paste this
-        //             Very long text goes here copy and paste this
-        //             Very long text goes here copy and paste this
-        //             Very long text goes here copy and paste this
-        //             Very long text goes here copy and paste this
-        //             Very long text goes here copy and paste this
-        //             Very long text goes here copy and paste this
-        //             Very long text goes here copy and paste this
-        //             Very long text goes here copy and paste this
-        //             Very long text goes here copy and paste this
-        //         </p>
-        //     </div>
-        // </main>
     )
 }
 
